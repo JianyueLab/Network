3748bab3463f57c2f59fb19baf83ceae7df70d75
Jianyue Hugo Liang <valor@jianyuelab.org>
2025-12-08 12:35:59 +0900

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,12 +21,8 @@
 
 		<h2 class="mt-4 text-2xl">Introduction</h2>
 		<p>
-			<a href="https://bgp.tools/as/215172" class="underline">AS 215172</a> is operated by
-			<a href="https://jhl.hk" class="underline">Jianyue Hugo Liang</a>, for learning and testing purpose.
-		</p>
-		<p>
-			<a href="https://bgp.tools/as/203168" class="underline">AS 203168</a> is operated by
-			<a href="https://jhl.hk" class="underline">Janyue Aosugi</a>, for business purpose.
+			<a href="https://bgp.tools/as/215172" class="underline">AS 215172</a> and <a href="https://bgp.tools/as/203168" class="underline">AS 203168</a> are operated by
+			<a href="https://jhl.hk" class="underline">Jianyue Hugo Liang, a.k.a. Janyue Aosugi</a>, for learning and testing purpose.
 		</p>
 
 		<h2 class="mt-4 text-2xl">Locations</h2>
